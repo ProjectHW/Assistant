ee4383e8b824957cf33f5234e19c0026b96ff6d9
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2022-04-05 22:39:05 +0300

    dev: Добавил в проект фронта библиотеку Material

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotesComponent } from './notes/notes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
