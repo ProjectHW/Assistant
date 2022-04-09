fa6efd5c04ace401c252780e9f12d3467401ae7a
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2022-04-09 13:19:31 +0300

    Добавил компонент notes

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotesComponent } from './notes/notes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { NotesModule } from './notes/notes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NotesComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    NotesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
